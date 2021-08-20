9ecf8f8307d48bf671d35c6f015936467ab64541
leomza <leo_mza1991@hotmail.com>
2021-08-20 09:46:10 +0300

    Added routes

--- server.dev.js
+++ server.dev.js
@@ -4,6 +4,9 @@ var express = require("express");
 
 var app = express();
 var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.send('<h1>I am alive!!!!!</h1>');
+});
 app.listen(port, function () {
   console.log("Listening on port: ".concat(port));
 });
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/',(req, res)=>{
+  res.send('<h1>I am alive!!!!!</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
