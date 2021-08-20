b5fc525bd49635d4332e6992ed008edbdfa84eec
leomza <leo_mza1991@hotmail.com>
2021-08-20 10:16:27 +0300

    Changes

--- server.dev.js
+++ server.dev.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express["static"]('public'));
+app.use(express["static"]('public/dist'));
 /* app.get('/',(req, res)=>{
   res.send('<h1>I am alive!!!!!</h1>')
 }) */
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.static('public/dist'));
+
 
 /* app.get('/',(req, res)=>{
   res.send('<h1>I am alive!!!!!</h1>')
